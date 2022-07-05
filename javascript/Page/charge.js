9f494183140637207ca9e8c43261d78c4a71afe3
YeonWooKim <kyw@emfoplus.co.kr>
2022-07-05 17:34:22 +0900

    Servcie, Charge 페이지 완료

--- javascript/Page/charge.js
+++ javascript/Page/charge.js
@@ -16,92 +16,45 @@ function _defineProperty(obj, key, value) {
     return obj;
 }
 
-var swiper_bottom_banner = new Swiper('.banner-bottom-container', (_Swiper = {
-    // effect: 'coverflow', // 커버플로우 효과 사용
-    // coverflowEffect:
-    // {
-    //     slideShadows: true, // 슬라이더 그림자 : 3D 효과를 강조하기 위한 회전시 흐릿한 효과
-    //     rotate: 50, // 슬라이더 회전 각 : 클수록 슬라이딩시 회전이 커짐
-    //     stretch: 0, // 슬라이더간 거리(픽셀) : 클수록 슬라이더가 서로 많이 겹침
-    //     depth: 100, // 깊이 효과값 : 클수록 멀리있는 느낌이 강해짐
-    //     modifier: 1, // 효과 배수 : 위 숫자값들에 이 값을 곱하기 처리하여 효과를 강하게 처리함
-    // },
-    observer: true,
-    observeParents: true,
-    centerInsufficientSlides: true,
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-    speed: 1500,
-    slidesPerView: 3,
-    spaceBetween: 20
-}, _defineProperty(_Swiper, "centeredSlides", true), _defineProperty(_Swiper, "simulateTouch", true), _defineProperty(_Swiper, "loop", true), _defineProperty(_Swiper, "slideToClickedSlide", true), _defineProperty(_Swiper, "autoplay", {
-    delay: 3000,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true
-}), _defineProperty(_Swiper, "breakpoints", {
-    768: {
-        spaceBetween: 40
-    }
-}), _Swiper));
+var banner_swiper1;
 
-$('.swiper-slide').on('mouseover', function () {
-    swiper_bottom_banner.autoplay.stop();
-});
+var bullet = [
+    `<span>기업</span>`,
+    `<span>개인</span>`,
+    `<span>대량발송</span>`,
+];
 
-$('.swiper-slide').on('mouseout', function () {
-    swiper_bottom_banner.autoplay.start();
-});
-
-swiper_bottom_banner.on('slideChange', function () {
-
-    // console.log("index");
-
-    var index = swiper_bottom_banner.activeIndex;
-    var active_slide = $('.banner-bottom-slide').eq(index); // 활성화 된 슬라이드
-
-    var non_active_slide = $('.banner-bottom-slide').eq(index).siblings(); // 비활성화 된 슬라이드
-
-    active_slide.addClass('banner-bottom-slide-active');
-    active_slide.animate({
-        opacity: "1"
-    }, 300);
-    non_active_slide.removeClass('banner-bottom-slide-active');
-    non_active_slide.animate({
-        opacity: "0.1"
-    }, 300);
-
-    if (index === 7) {
-        showsubimg(0);
-    } else {
-        showsubimg(index - 3);
-    }
-});
-
-function showsubimg(index) {
-    $('.main_content2 .contents').removeClass('banner-bottom-slide-select');
-    $('.main_content2 .contents').eq(index).addClass('banner-bottom-slide-select');
-    $('.main_content2 .contents').each(function () {
-        if ($(this).hasClass('banner-bottom-slide-select')) {
-            $(this).addClass('overcolor');
-        } else {
-            $(this).removeClass('overcolor');
-        }
+function swiper_banner1() {
+    banner_swiper1 = new Swiper('.swiper-container2', {
+        // slidesPerView : 2, // 동시에 보여줄 슬라이드 갯수
+        // spaceBetween : 10, // 슬라이드간 간격
+        observer: true,
+        observeParents: true,
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 4000000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+        },
+        pagination: {
+            el: '.swiper-pagination-custom',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return `<div class="colmun_wrapper bullet_button ${className}">${bullet[index]}</div>`
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
-    // swiper_bottom_banner.slideTo(index + 3, 1000);
 }
 
 $(document).ready(function () {
-
-    showsubimg(0); // 활성화 된 슬라이드에 클래스 추가
-
-    $('.banner-bottom-slide').eq(swiper_bottom_banner.activeIndex).addClass('banner-bottom-slide-active'); // 활성화 된 슬라이드 애니메이션
-
-    $('.banner-bottom-slide').eq(swiper_bottom_banner.activeIndex).animate({
-        opacity: "1"
-    }, 300); // 비활성화 된 슬라이드 애니메이션
-
-    selectEmail_Event();
-    
+    // 배너 슬라이드 실행
+    swiper_banner1();
 });
 
 function inquiry_layout(){
--- javascript/Page/service-globalmsg.js
+++ javascript/Page/service-globalmsg.js
@@ -14,23 +14,74 @@ function _defineProperty(obj, key, value) {
     return obj;
 }
 
-var banner_swiper;
+var banner_swiper1;
+var banner_swiper2;
 
-function swiper_banner() {
-    banner_swiper = new Swiper('.swiper-container4', {
+var bullet = [
+    `<div class="column_wrapper info_title">
+        <p>높은 도달</p>
+        <p>성공률</p>
+    </div>
+    <div class="row_wrapper icon_content">
+        <img class="icon_img" src="../image/service/icon_rate_on.png">
+    </div>`,
+    `<div class="column_wrapper info_title">
+        <p>발송 실패 건</p>
+        <p>자동환급</p>
+    </div>
+    <div class="row_wrapper icon_content">
+        <img class="icon_img" src="../image/service/icon_refund_on.png">
+    </div>`,
+    `<div class="column_wrapper info_title">
+        <p>국가별</p>
+        <p>언어 전송 가능</p>
+    </div>
+    <div class="row_wrapper icon_content">
+        <img class="icon_img" src="../image/service/icon_global_on.png">
+    </div>`
+];
+
+function swiper_banner1() {
+    banner_swiper1 = new Swiper('.swiper-container2', {
+        effect: "fade",
+        fadeEffect: { 
+        crossFade: true 
+        },
+        observer: true,
+        observeParents: true,
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 4000000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+        },
+        pagination: {
+            el: '.swiper-pagination-custom',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return `<div class="colmun_wrapper pg_box ${className}">${bullet[index]}</div>`
+            }
+        },
+    });
+}
+
+function swiper_banner2() {
+    banner_swiper2 = new Swiper('.swiper-container4', {
         observer: true,
         observeParents: true,
         direction: 'horizontal',
         loop: true,
         speed: 1200,
         autoplay: {
-            delay: 4000,
+            delay: 400000,
             disableOnInteraction: false,
             pauseOnMouseEnter: true
         },
         pagination: {
             el: '.swiper-pagination',
-            clickable: true
+            clickable: true,
         },
         navigation: {
             nextEl: '.swiper-button-next',
@@ -41,5 +92,6 @@ function swiper_banner() {
 
 $(document).ready(function () {
     // 배너 슬라이드 실행
-    swiper_banner();
+    swiper_banner1();
+    swiper_banner2();
 });
